c14efdd623dde4ead44e8f94e0b5a7ce57527bc2
Nehagarg17 <Nehagarg179@gmail.com>
2023-09-10 21:44:23 +0530

    deploy script

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/home/Home";
 import Profile from "./containers/profile/Profile";
-import AppContext from './AppContext';
-import { useState, Suspense } from 'react';
+import AppContext from "./AppContext";
+import { useState, Suspense } from "react";
 
 function App() {
   const [appState, setAppState] = useState({});
-  console.log('appState: ', appState);
+  console.log("appState: ", appState);
 
   return (
     <div className="App">
       <AppContext.Provider value={{ appState, setAppState }}>
-        <Suspense fallback={'loading...'}>
-          <Router>
+        <Suspense fallback={"loading..."}>
+          <Router basename={process.env.PUBLIC_URL}>
             <Route exact path="/" component={Home}></Route>
             <Route path="/home" component={Home}></Route>
             <Route path="/:userId/:type" component={Profile}></Route>
